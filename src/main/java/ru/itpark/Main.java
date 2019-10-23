04566d834a1f36987cf336eb6dfb7497afd46b99
Almir <almir.almiev@tatar.ru>
2019-10-23 17:02:44 +0300

    travis must build up

--- src/main/java/ru/itpark/Main.java
+++ src/main/java/ru/itpark/Main.java
@@ -1,10 +1,6 @@
 package ru.itpark;
 
-import ru.itpark.model.CommunicationConditions;
-import ru.itpark.model.HeadAndSubhead;
 import ru.itpark.model.OnWatchTariff;
-import ru.itpark.util.Internet;
-import ru.itpark.util.PhoneCommunication;
 
 public class Main {
     public static void main(String[] args) {
--- src/main/java/ru/itpark/model/OnWatchTariff.java
+++ src/main/java/ru/itpark/model/OnWatchTariff.java
@@ -4,27 +4,9 @@ import ru.itpark.util.Internet;
 import ru.itpark.util.PhoneCommunication;
 
 public class OnWatchTariff extends Tariff {
-    private PhoneCommunication phoneCommunication = new PhoneCommunication(1200);
-    private Internet internet = new Internet();
-    private int countsSms = 300;
-    private HeadAndSubhead head = new HeadAndSubhead("On! Watch");
-    private CommunicationConditions conditions = new CommunicationConditions(phoneCommunication, internet, countsSms);
-    private boolean choose = true;
-    private boolean about = true;
 
-    public OnWatchTariff(){
-        super();
-    }
-
-    @Override
-    public String toString() {
-        return "OnWatchTariff{" +
-                "id=" + id +
-                ", title=" + title +
-                ", conditions=" + conditions +
-                ", price=" + price +
-                ", choose=" + choose +
-                ", about=" + about +
-                '}';
+    public OnWatchTariff() {
+        super(1, new HeadAndSubhead("On! Watch"), new CommunicationConditions(new PhoneCommunication(1200),
+                new Internet(), 300), 750, true, true);
     }
 }
